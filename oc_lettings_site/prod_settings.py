99a724ca4df0c968bbc61062ebe07964299c1899
AnaisG14 <gatard.anais@gmail.com>
2022-07-09 17:47:23 +0200

    modify config.yml l.73

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -1,8 +1,7 @@
 import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.core.exceptions import ImproperlyConfigured
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
