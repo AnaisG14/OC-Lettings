304c3b1febf22f01fb166fddbe9e0bae2d6a349c
AnaisG14 <gatard.anais@gmail.com>
2022-07-13 13:26:00 +0200

    change SECRET_KEY

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -1,7 +1,7 @@
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-from django.core.exceptions import ImproperlyConfigured
+# from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,20 +10,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-def get_environment_var(var_name, default_value=None):
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        if default_value is None:
-            raise ImproperlyConfigured(f"Set the {var_name} environment variable.")
-        else:
-            return default_value
+# def get_environment_var(var_name, default_value=None):
+#     try:
+#         return os.environ[var_name]
+#     except KeyError:
+#         if default_value is None:
+#             raise ImproperlyConfigured(f"Set the {var_name} environment variable.")
+#         else:
+#             return default_value
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_environment_var('SECRET_KEY',
-                                 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s')
+# SECRET_KEY = get_environment_var('SECRET_KEY',
+#                                  'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s')
 
+SECRET_KEY = os.environ.get('SECRET_KEY',
+                            default='fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
